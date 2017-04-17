5e14b1f6b1fbfc9c914915bcd24d5d262b817c13
Mihir-mohaN <mimo16cs@cmrit.ac.in>
2017-04-17 18:45:18 +0530

    Update strcpy.c

--- week5/strcpy.c
+++ week5/strcpy.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
-#include<string.h>
 
-void string_copy(char a[],char b[]);
+void string_copy(char *,char *,int );
 
 int main(){
 	char a[10];
+	int i;
 	printf("enter string:");
 	fgets(a,sizeof(a),stdin);
-	char b[strlen(a)-1];
-	string_copy(a,b);
-	printf("original string:");
+	for(i=0;a[i]!='\0';i++);
+	char b[i];
+	string_copy(a,b,i);
+	printf("original string:\n");
 	fputs(a,stdout);
-	printf("copied string:");
+	printf("copied string:\n");
 	fputs(b,stdout);
 	return 0;
 }
 
-void string_copy(char a[],char b[]){
-	int x=strlen(a);
-	for(int i=0;i<x;i++)
-		b[i]=a[i];
+void string_copy(char a[],char b[],int i){
+	for(int k=0;k<i;k++)
+		b[k]=a[k];
 }
