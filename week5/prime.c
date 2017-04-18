700897ff781826ec8d43c290a8d519121a645e8a
Mihir-mohaN <mimo16cs@cmrit.ac.in>
2017-04-18 20:37:12 +0530

    Update prime.c

--- week5/prime.c
+++ week5/prime.c
@@ -23,6 +23,13 @@ int isprime(int x){
 	if(x%7==0&&x!=7) flag++;
 	if(x%11==0&&x!=11) flag++;
 	if(x%13==0&&x!=13) flag++;
+	if(x%17==0&&x!=17) flag++;
+	if(x%23==0&&x!=23) flag++;
+	if(x%29==0&&x!=29) flag++;
+	if(x%31==0&&x!=31) flag++;
+	if(x%37==0&&x!=37) flag++;
+	if(x%41==0&&x!=41) flag++;
+	if(x%43==0&&x!=43) flag++;
 	
 	if(flag>0) return 0;
 	else return 1;	
