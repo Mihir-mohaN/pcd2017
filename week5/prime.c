fd9998837f2ab5a6278e18e9bce364c6e6b045f8
Mihir-mohaN <mimo16cs@cmrit.ac.in>
2017-04-18 18:26:33 +0530

    Update prime.c

--- week5/prime.c
+++ week5/prime.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
+
 int isprime(int);
+
 int main(){
 	int num;
 	printf("enter a number to check whether it is prime:");
@@ -12,8 +14,16 @@ int main(){
 }
 
 int isprime(int x){
-	if(x%2==0||x%3==0||x%5==0||x%7==0)
-		return 0;
-	else
-		return 1;
+	if(x==0||x==1) return 0;
+	
+	int flag=0;
+	if(x%2==0&&x!=2) flag++;
+	if(x%3==0&&x!=3) flag++;
+	if(x%5==0&&x!=5) flag++;
+	if(x%7==0&&x!=7) flag++;
+	if(x%11==0&&x!=11) flag++;
+	if(x%13==0&&x!=13) flag++;
+	
+	if(flag>0) return 0;
+	else return 1;	
 }
