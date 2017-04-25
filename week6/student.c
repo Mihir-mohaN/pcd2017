20e3f0a6555ff79f80f8730bc28d51dc87746f8f
Mihir-mohaN <mimo16cs@cmrit.ac.in>
2017-04-25 21:49:25 +0530

    Create student.c

---
+++ week6/student.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+
+typedef struct{
+	int roll,marks;
+	char name[20],grade[2];
+}student;
+
+void input(int,student *);
+int search(int,student *,char *);
+
+int main(){
+	student *s;
+	int n;
+	printf("enter how many students:");
+	scanf("%d",&n);
+	input(n,s);
+	
+	while(1){
+		char sname[20];
+		printf("enter the name you want to search for:");
+		scanf("%s",sname);
+		
+		if(search(n,s,sname)!=0)
+			printf("the marks is %d\n",s[search(n,s,sname)].marks);
+		else
+			printf("not found\n");
+	}
+	return 0;
+}
+
+void input(int n,student *s){
+	for(int i=0;i<n;i++){
+		printf("enter the roll, name, marks and grade of student %d:",i+1);
+		scanf("%d%s%d%s",&s[i].roll,s[i].name,&s[i].marks,s[i].grade);
+	}
+}
+
+int search(int n,student *s,char sname[]){
+	for(int i=0;i<n;i++){
+		if(strcmp(s[i].name,sname)==0)
+			return i;
+	}
+	return 0;
+}
