512043fb5dc6fff859efdf185c96457ef39ca19a
Mihir-mohaN <mimo16cs@cmrit.ac.in>
2017-04-25 21:48:57 +0530

    Update file.c

--- week6/file.c
+++ week6/file.c
@@ -1,18 +1,34 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
 	char stna[100],usn[10],op[100];
 	FILE *stud_name=fopen("studentname.txt","r");
 	FILE *usnum=fopen("usn.txt","r");
-	FILE *o_p=fopen("output.txt","w+");
+	FILE *o_p=fopen("output.txt","w");
 	
+	while(!feof(stud_name)&&!feof(usnum)){
 	fgets(stna,sizeof(stna),stud_name);
 	fgets(usn,sizeof(usn),usnum);
+		size_t sn= strlen(stna)-1;
+		if (stna[sn] == '\n')
+		stna[sn] = '\0';
 	
-	fputs(stna,o_p);
-	fputs(usn,o_p);
+		size_t un= strlen(usn)-1;
+		if (usn[un] == '\n')
+		usn[un] = '\0';
+	fprintf(o_p,"%s10%s\n",stna,usn);
+	}
 	
-	while(fgets(op,sizeof(op),o_p)!=NULL)
-		puts(op);
+	fclose(stud_name);
+    fclose(usnum);
+    fclose(o_p);
+    
+	o_p=fopen("output.txt","r");
+	
+	while(!feof(o_p)){
+	fgets(op,sizeof(op),o_p);
+	fputs(op,stdout);
+	}
 	
 	return 0;
 }
