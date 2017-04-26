ff9a856dc53d75996714c591a2c84cbe1b597d55
Mihir-mohaN <mimo16cs@cmrit.ac.in>
2017-04-26 20:55:53 +0530

    Update student.c

--- week6/student.c
+++ week6/student.c
@@ -10,7 +10,7 @@ void input(int,student *);
 int search(int,student *,char *);
 
 int main(){
-	student *s;
+	student s[100];
 	int n;
 	printf("enter how many students:");
 	scanf("%d",&n);
@@ -30,14 +30,14 @@ int main(){
 }
 
 void input(int n,student *s){
-	for(int i=0;i<n;i++){
-		printf("enter the roll, name, marks and grade of student %d:",i+1);
+	for(int i=1;i<=n;i++){
+		printf("enter the roll, name, marks and grade of student %d:",i);
 		scanf("%d%s%d%s",&s[i].roll,s[i].name,&s[i].marks,s[i].grade);
 	}
 }
 
 int search(int n,student *s,char sname[]){
-	for(int i=0;i<n;i++){
+	for(int i=1;i<=n;i++){
 		if(strcmp(s[i].name,sname)==0)
 			return i;
 	}
